b3643d1d6dede63c5b991f62f1d7d0d6a7670ae9
Suprim-Shakya <suprimshakyaofficial@gmail.com>
2023-12-10 19:31:09 +0545

    made suitable formatting

--- TravelApp/src/views/screens/HomeScreen.js
+++ TravelApp/src/views/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 import COLORS from '../../consts/colors';
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -6,45 +6,50 @@ import { ScrollView, TextInput } from 'react-native-gesture-handler';
 
 const HomeScreen = ({navigation}) => {
   const categoryIcon = [
-    <Icon name="restaurant" size={25} color={COLORS.grey} />,
-    <Icon name="flights_and_hotels" size={25} color={COLORS.grey} />,
-    <Icon name="wc" size={25} color={COLORS.grey} />,
+    <Icon name="restaurant" size={25} color={COLORS.dark}/>, 
+    <Icon name="emergency" size={25} color={COLORS.dark} />,
+    <Icon name="wc" size={25} color={COLORS.dark} />,
+    <Icon name="atm" size={25} color={COLORS.dark} />,
   ]
 
   const ListCategories = () => {
-    return <View style={styles.categoryContainer}>
+    return (
+    <View style={styles.categoryContainer}>
       {categoryIcon.map((icon, index) => (
-        <View key={index}> {icon} </View>
-      )
-      
-      )}
+        <View key={index} style={styles.iconContainer}>
+          {icon} 
+        </View>
+      ))}
     </View>
-  }
+  )};
+
+ 
+
 
   return (
     <SafeAreaView style={{flex:1}}>
       <StatusBar translucent={false} backgroundColor={COLORS.primary} />
-    <View style={styles.header}>
-      <Icon name="sort" size={28} color='#FFF'/>
-      <Text style={styles.title}>Travel Guide</Text>
-      <Icon name="notifications" size={28} color={COLORS.white} />
-    </View>
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <View style={{backgroundColor : COLORS.primary, height: 120}}>
-        <View>
-          <Text style={styles.text}>Discover the Best</Text>
-          <Text style={styles.text}>Sites to Travel</Text>
-            <View style={styles.inputcontainer}>
-            <Icon name="search" size={28} style={styles.icons}/>
-            <TextInput placeholder='Search heritage' style={{color: COLORS.grey}}/>
-            </View>
+        <View style={styles.header}>
+          <Icon name="sort" size={28} color='#FFF' style={{marginHorizontal:10}}/>
+          <Text style={styles.title}>Travel Guide</Text>
+          <Icon name="notifications" size={28} color={COLORS.white} style={{marginHorizontal:10}}/>
         </View>
-        
-      </View>
-      <ListCategories>
-
-      </ListCategories>
-    </ScrollView>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{backgroundColor : COLORS.primary, height: 120}}>
+          <View>
+            <Text style={styles.text}>Discover the Best</Text>
+            <Text style={styles.text}>Sites to Travel</Text>
+              <View style={styles.inputcontainer}>
+                <Icon name="search" size={28} style={styles.icons}/>
+                <TextInput placeholder="Search"/>
+              </View>
+          </View>
+          
+        </View>
+        <ListCategories>
+          <Text style={styles.sectionTitle}> Trending </Text>
+        </ListCategories>
+      </ScrollView>
     </SafeAreaView>
   )
 }
@@ -75,6 +80,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 90,
     alignSelf: 'center',
+    flexDirection: 'row',
   },
   icons:{
     marginTop: 15,
@@ -85,5 +91,13 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  iconContainer:{
+    height: 60,
+    width: 60,
+    backgroundColor: COLORS.secondary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
   }
-})
+});
