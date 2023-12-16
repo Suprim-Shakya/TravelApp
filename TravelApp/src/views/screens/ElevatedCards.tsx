5d5ad654d69e6c037cf1895586829b44bf77ec0a
Suprim Shakya <121931537+Suprim-Shakya@users.noreply.github.com>
2023-12-16 21:27:34 +0545

    converted ElevatedCards.tsx to js

--- TravelApp/src/views/screens/ElevatedCards.tsx
+++ TravelApp/src/views/screens/ElevatedCards.tsx
@@ -1,91 +1,96 @@
+import React, { useState } from "react"
+import {
+  View,
+  ScrollView,
+  TouchableOpacity,
+  TouchableHighlight,
+  Text,
+  Image,
+  Modal,
+  StyleSheet
+} from "react-native"
 
-
-import React, { useState } from 'react';
-import { View, ScrollView, TouchableOpacity,TouchableHighlight, Text, Image, Modal, StyleSheet } from 'react-native';
-
-// Define the data structure
-interface Item {
-  id: string;
-  title: string;
-  imageUri: string;
-  description: string;
-}
-
-interface Data {
-  [key: string]: Item[];
-}
-
-const data: Data = {
+const data = {
   activities: [
     {
-      id: '1',
-      title: 'Archery',
-      imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTunvm-6LiAf3T5usU7wfGzRdJPHqQJTa0MAUnB0X9Ct8V5T1-ECVmnWKkGtc_2Wwlgz8k&usqp=CAU',
-      description: 'Interested in bow and arrow? Then you must visit this place.',
+      id: "1",
+      title: "Archery",
+      imageUri:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTunvm-6LiAf3T5usU7wfGzRdJPHqQJTa0MAUnB0X9Ct8V5T1-ECVmnWKkGtc_2Wwlgz8k&usqp=CAU",
+      description:
+        "Interested in bow and arrow? Then you must visit this place."
     },
-    { id: '2', title: 'Zoo', imageUri: 'https://www.stunningnepal.com/wp-content/uploads/2022/06/Kathmandu-Fun-Park.jpg', description: 'Description for Activity 1' },
+    {
+      id: "2",
+      title: "Zoo",
+      imageUri:
+        "https://www.stunningnepal.com/wp-content/uploads/2022/06/Kathmandu-Fun-Park.jpg",
+      description: "Description for Activity 1"
+    }
   ],
   cuisines: [
-    { id: '1', title: 'Cuisine 1', imageUri: 'cuisine1.jpg', description: '' },
-    { id: '2', title: 'Cuisine 2', imageUri: 'cuisine2.jpg', description: 'Description for Cuisine 2' },
+    { id: "1", title: "Cuisine 1", imageUri: "cuisine1.jpg", description: "" },
+    {
+      id: "2",
+      title: "Cuisine 2",
+      imageUri: "cuisine2.jpg",
+      description: "Description for Cuisine 2"
+    }
   ],
   trekking: [
-    { id: '1', title: 'Trek 1', imageUri: 'trek1.jpg', description: 'Description for Trek 1' },
-    { id: '2', title: 'Trek 2', imageUri: 'trek2.jpg', description: 'Description for Trek 2' },
-  ],
-};
-
-interface CustomCardProps {
-  title: string;
-  description: string;
-  imageUri: string;
-  selected: boolean;
-  onPress: () => void;
-  onModalPress: () => void;
+    {
+      id: "1",
+      title: "Trek 1",
+      imageUri: "trek1.jpg",
+      description: "Description for Trek 1"
+    },
+    {
+      id: "2",
+      title: "Trek 2",
+      imageUri: "trek2.jpg",
+      description: "Description for Trek 2"
+    }
+  ]
 }
 
-const CustomCard: React.FC<CustomCardProps> = ({
+const CustomCard = ({
   title,
   description,
   imageUri,
   selected,
   onPress,
-  onModalPress,
+  onModalPress
 }) => {
   const cardStyles = {
     padding: 3,
     margin: 2,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     width: 300,
-    height: 250,
-  };
+    height: 250
+  }
 
   return (
     <ScrollView>
       <View style={cardStyles}>
         <Image source={{ uri: imageUri }} style={{ width: 293, height: 170 }} />
-        <Text style={{ fontWeight: 'bold', color: 'red', textAlign: 'center' }}>{title}</Text>
-        <Text style={{ fontWeight: 'bold', color: 'green', textAlign: 'justify' }}>{description}</Text>
+        <Text style={{ fontWeight: "bold", color: "red", textAlign: "center" }}>
+          {title}
+        </Text>
+        <Text
+          style={{ fontWeight: "bold", color: "green", textAlign: "justify" }}
+        >
+          {description}
+        </Text>
         <TouchableOpacity onPress={onModalPress}>
-          <Text style={{ color: 'black', textAlign: 'center' }}>View More</Text>
+          <Text style={{ color: "black", textAlign: "center" }}>View More</Text>
         </TouchableOpacity>
       </View>
     </ScrollView>
-  );
-};
-
-interface ModalContentProps {
-  title: string;
-  closeModal: () => void;
+  )
 }
 
-interface ModalContentProps {
-  title: string;
-  closeModal: () => void;
-}
-
-const ModalContent: React.FC<ModalContentProps> = ({ title, closeModal }) => {
-  const selectedData = data[title];
+const ModalContent = ({ title, closeModal }) => {
+  const selectedData = data[title]
 
   if (!selectedData || selectedData.length === 0) {
     return (
@@ -95,13 +100,13 @@ const ModalContent: React.FC<ModalContentProps> = ({ title, closeModal }) => {
           <Text style={styles.modalCloseButton}>Close</Text>
         </TouchableOpacity>
       </View>
-    );
+    )
   }
 
   return (
     <View style={styles.modalContainer}>
       <Text style={styles.modalTitle}>{title}</Text>
-      {selectedData.map((item) => (
+      {selectedData.map(item => (
         <View key={item.id}>
           <Text style={styles.modalText}>{item.description}</Text>
         </View>
@@ -110,57 +115,87 @@ const ModalContent: React.FC<ModalContentProps> = ({ title, closeModal }) => {
         <Text style={styles.modalCloseButton}>Close</Text>
       </TouchableOpacity>
     </View>
-  );
-};
+  )
+}
 
-const App: React.FC = () => {
-  const [selectedButton, setSelectedButton] = useState('activities');
-  const [selectedCard, setSelectedCard] = useState<Item | null>(null);
-  const [isModalVisible, setModalVisible] = useState(false);
+const App = () => {
+  const [selectedButton, setSelectedButton] = useState("activities")
+  const [selectedCard, setSelectedCard] = useState(null)
+  const [isModalVisible, setModalVisible] = useState(false)
 
-  const selectedData: Item[] = data[selectedButton] || [];
+  const selectedData = data[selectedButton] || []
 
-  const handleCardPress = (card: Item) => {
-    setSelectedCard(selectedCard?.id === card.id ? null : card);
-  };
+  const handleCardPress = card => {
+    setSelectedCard(selectedCard?.id === card.id ? null : card)
+  }
 
   const handleModalPress = () => {
-    setModalVisible(true);
-  };
+    setModalVisible(true)
+  }
 
   const closeModal = () => {
-    setModalVisible(false);
-  };
+    setModalVisible(false)
+  }
 
   return (
     <View>
       <View style={styles.buttonContainer}>
-      <TouchableHighlight
-        style={[styles.button, selectedButton === 'activities' && styles.selectedButton]}
-        underlayColor="#DDDDDD"
-        onPress={() => setSelectedButton('activities')}
-      >
-        <Text style={[styles.buttonText, selectedButton === 'activities' && styles.selectedButtonText]}>Activities</Text>
-      </TouchableHighlight>
+        <TouchableHighlight
+          style={[
+            styles.button,
+            selectedButton === "activities" && styles.selectedButton
+          ]}
+          underlayColor="#DDDDDD"
+          onPress={() => setSelectedButton("activities")}
+        >
+          <Text
+            style={[
+              styles.buttonText,
+              selectedButton === "activities" && styles.selectedButtonText
+            ]}
+          >
+            Activities
+          </Text>
+        </TouchableHighlight>
 
-      <TouchableHighlight
-        style={[styles.button, selectedButton === 'cuisines' && styles.selectedButton]}
-        underlayColor="#DDDDDD"
-        onPress={() => setSelectedButton('cuisines')}
-      >
-        <Text style={[styles.buttonText, selectedButton === 'cuisines' && styles.selectedButtonText]}>Cuisines</Text>
-      </TouchableHighlight>
+        <TouchableHighlight
+          style={[
+            styles.button,
+            selectedButton === "cuisines" && styles.selectedButton
+          ]}
+          underlayColor="#DDDDDD"
+          onPress={() => setSelectedButton("cuisines")}
+        >
+          <Text
+            style={[
+              styles.buttonText,
+              selectedButton === "cuisines" && styles.selectedButtonText
+            ]}
+          >
+            Cuisines
+          </Text>
+        </TouchableHighlight>
 
-      <TouchableHighlight
-        style={[styles.button, selectedButton === 'trekking' && styles.selectedButton]}
-        underlayColor="#DDDDDD"
-        onPress={() => setSelectedButton('trekking')}
-      >
-        <Text style={[styles.buttonText, selectedButton === 'trekking' && styles.selectedButtonText]}>Trekking</Text>
-      </TouchableHighlight>
-    </View>
+        <TouchableHighlight
+          style={[
+            styles.button,
+            selectedButton === "trekking" && styles.selectedButton
+          ]}
+          underlayColor="#DDDDDD"
+          onPress={() => setSelectedButton("trekking")}
+        >
+          <Text
+            style={[
+              styles.buttonText,
+              selectedButton === "trekking" && styles.selectedButtonText
+            ]}
+          >
+            Trekking
+          </Text>
+        </TouchableHighlight>
+      </View>
       <ScrollView horizontal>
-        {selectedData.map((item) => (
+        {selectedData.map(item => (
           <CustomCard
             key={item.id}
             title={item.title}
@@ -180,63 +215,62 @@ const App: React.FC = () => {
         </View>
       </Modal>
     </View>
-  );
-};
+  )
+}
 
-const styles =StyleSheet.create({
+const styles = StyleSheet.create({
   modalContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)"
   },
   modalTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    color: 'white',
+    color: "white"
   },
   modalText: {
     fontSize: 10,
-    color: 'white',
+    color: "white"
   },
   modalCloseButton: {
     fontSize: 16,
-    color: 'white',
-    marginTop: 10,
+    color: "white",
+    marginTop: 10
   },
-  NavTab:{
-    color:'red',
-    backgroundColor:'green',
-    width:100,
-    height:30,
-    alignItems:'center',
-    textAlign:'center',
-    verticalAlign:'middle',
-    textAlignVertical:'center',
+  NavTab: {
+    color: "red",
+    backgroundColor: "green",
+    width: 100,
+    height: 30,
+    alignItems: "center",
+    textAlign: "center",
+    verticalAlign: "middle",
+    textAlignVertical: "center"
   },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
   },
   button: {
     flex: 1,
     padding: 10,
     borderRadius: 5,
-    backgroundColor:'#e0e0e0',
+    backgroundColor: "#e0e0e0"
   },
   selectedButton: {
-    backgroundColor: '#222222', // Set your desired highlight color
+    backgroundColor: "#222222" // Set your desired highlight color
   },
   buttonText: {
-    color: 'black', // Set your desired text color
-    textAlign: 'center', // Center the text within the button
+    color: "black", // Set your desired text color
+    textAlign: "center" // Center the text within the button
   },
   selectedButtonText: {
-    color: 'white', // Set your selected text color
-  },
-});
+    color: "white" // Set your selected text color
+  }
+})
 
-export default App;
-
+export default App
