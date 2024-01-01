97bdc10a907716d13fc22a11a4ef614d3e65bea7
Saurav Khanal <hi.sauravkhanal@gmail.com>
2024-01-01 23:59:44 +0545

    bottom nav gallery works

---
+++ APPX.jsx
@@ -0,0 +1,90 @@
+import React, { useRef } from 'react';
+import { Button } from 'react-native';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import DetailsScreen from './src/views/screens/DetailsScreen';
+import Detections from './src/componentsSaurav/Detections';
+import HomeScreen from './src/views/screens/HomeScreen';
+import Maps from './src/componentsSaurav/Maps';
+import BottomDrawer from './src/componentsSaurav/BottomDrawer';
+
+import RBSheet from 'react-native-raw-bottom-sheet';
+
+const HomeStack = createStackNavigator();
+
+const HomeScreenStack = () => {
+    return (
+        <HomeStack.Navigator screenOptions={{ headerShown: false }}>
+            <HomeStack.Screen name='HomeScreen' component={HomeScreen} />
+            <HomeStack.Screen name='DetailsScreen' component={DetailsScreen} />
+        </HomeStack.Navigator>
+    );
+};
+
+// const BottomDrawer = ({ refRBSheet }) => {
+//     return (
+//         <RBSheet ref={refRBSheet} closeOnDragDown={true} closeOnPressBack={true} customStyles={{ wrapper: { backgroundColor: 'rgba(0,0,0, 0)' }, draggableIcon: { backgroundColor: "#000" } }}>
+//             {/* <Button title='Open Camera' onPress={() => { openCamera }} />
+//             <Button title='Open Gallery' onPress={openGallery} /> */}
+//             <Button title='Cancel' onPress={() => refRBSheet.current.close()} />
+//         </RBSheet>
+//     )
+// }
+
+const ScanStack = createStackNavigator();
+
+const ScanScreenStack = ({ navigation }) => {
+    const refRBSheet = useRef();
+
+    return (
+        <ScanStack.Navigator screenOptions={{ headerShown: true }}>
+            {/* <ScanStack.Screen name='BottomDrawer'  component={BottomDrawer} /> */}
+            <ScanStack.Screen name='Detections' component={Detections}  />
+        </ScanStack.Navigator>
+    );
+};
+
+const App = () => {
+    const Tab = createBottomTabNavigator();
+    const refRBSheet = useRef();
+    return (
+        <NavigationContainer>
+            <Tab.Navigator screenOptions={{ headerShown: false }}>
+                <Tab.Screen
+                    name='Home'
+                    component={HomeScreenStack}
+                    options={{
+                        tabBarIcon: ({ color, size }) => <Icon name='home' color={color} size={size * 1.2} />,
+                    }}
+                />
+                <Tab.Screen
+                    name='Scan'
+                    component={ScanScreenStack} // this is prevented by listeners
+                    // initialParams={{openDrawer: ()=> refRBSheet.current.open()}}
+                    options={{
+                        tabBarIcon: ({ color, size }) => <Icon name='camera-alt' color={color} size={size * 1.2} />,
+                    }}
+                listeners={() => ({
+                    tabPress: e => {
+                        e.preventDefault();
+                        refRBSheet.current.open();
+                    }
+                })}
+                />
+                <Tab.Screen
+                    name='Maps'
+                    component={Maps}
+                    options={{ tabBarIcon: ({ color, size }) => <Icon name='map' color={color} size={size * 1.2} /> }}
+                />
+            </Tab.Navigator>
+            <BottomDrawer refRBSheet={refRBSheet} />
+        </NavigationContainer>
+    );
+};
+
+
+export default App;
---
+++ src/componentsSaurav/BottomDrawer.jsx
@@ -0,0 +1,127 @@
+import { View, Text, Button } from 'react-native'
+import React, { useRef, useState } from 'react'
+import RBSheet from 'react-native-raw-bottom-sheet';
+
+import { launchImageLibrary, launchCamera } from 'react-native-image-picker';
+import { API_ENDPOINT } from './config';
+import { useNavigation } from '@react-navigation/native';
+
+
+
+
+const BottomDrawer = ({refRBSheet }) => {
+
+    const navigation = useNavigation();
+    // const refRBSheet = useRef();
+    const [responseImageURL, setResponseImageURL] = useState('');
+    const [responseName, setResponseName] = useState('');
+
+    const handleDetection = ({detections, imageURL, numberOfDetection}) => {
+        navigation.navigate('Scan', { screen: 'Detections', params: { detections, imageURL, numberOfDetection} })
+    }
+
+    const openCamera = async () => {
+        const options = {};
+        try {
+            const callbackFxn = async (response) => {
+
+                if (response.didCancel) {
+                    return
+                } //user didn't click image
+
+
+                const formData = new FormData();
+                formData.append(
+                    'file', {
+                    uri: response.assets[0].uri,
+                    type: response.assets[0].type,
+                    name: response.assets[0].fileName,
+                });
+
+                await sendImageToServer(formData);
+            }
+            await launchCamera(options, callbackFxn);
+
+        } catch (error) {
+            console.error('Error occurred while sending image to server from camera: ', error);
+            Alert.alert("Couldn't send image to server.")
+        }
+    }
+
+
+    const openGallery = async () => {
+        try {
+
+            const callbackFxn = async (response) => {
+
+                if (response.didCancel) {
+                    return
+                } //user didn't select image
+
+
+                const formData = new FormData();
+                formData.append(
+                    'file', {
+                    uri: response.assets[0].uri,
+                    type: response.assets[0].type,
+                    name: response.assets[0].fileName,
+                });
+
+                await sendImageToServer(formData);
+            }
+            await launchImageLibrary({ mediaType: 'photo', selectionLimit: 1 }, callbackFxn);
+
+        } catch (error) {
+            console.error('Error occurred while sending image to server from gallery: ', error);
+            Alert.alert("Couldn't send image to server.")
+        }
+    }
+
+
+    const sendImageToServer = async (formData) => {
+        // setIsLoading(true);
+        try {
+            const response = await fetch(API_ENDPOINT, {
+                method: 'post',
+                body: formData,
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+
+            // if (!response.ok) {
+            //     throw new Error(`HTTP error! Status: ${response.status}`);
+            // }
+
+            const responseJson = await response.json();
+
+            handleDetection({ ...responseJson.data })
+
+            // console.log(responseJson)
+            // setResponse(responseJson.message);
+            // setResponseImageURL(responseJson.data.imageURL);
+            // responseJson.data.detections[0] ? setResponseName(responseJson.data.detections[0].name) : setResponseName('No detections')
+
+        } catch (error) {
+            if (error.message === 'Network request failed') {
+                Alert.alert('Internet unstable', 'Please check your internet connection')
+            } else {
+                console.error('Error occurred on post request send to server: ', error)
+            }
+
+        } finally {
+            // setIsLoading(false)
+        }
+    }
+
+
+    return (
+        <RBSheet ref={refRBSheet} closeOnDragDown={true} closeOnPressBack={true} customStyles={{ wrapper: { backgroundColor: 'rgba(0,0,0, 0.8)' }, draggableIcon: { backgroundColor: "#000" } }}>
+            <Button title='Open Camera' onPress={() => { openCamera }} />
+            <Button title='Open Gallery' onPress={openGallery} />
+            <Button title='Cancel' onPress={() => refRBSheet.current.close()} />
+        </RBSheet>
+    )
+}
+
+export default BottomDrawer
