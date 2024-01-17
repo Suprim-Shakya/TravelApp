0cb547c9350b8c4eeff9f4aeafe1816ac971c909
Suprim-Shakya <suprimshakyaofficial@gmail.com>
2024-01-17 15:09:51 +0545

    deleted files

--- src/componentsSaurav/redux/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import bookmarkReducer from '../features/bookmarkSlice'
-
-const store = configureStore({
-    reducer: bookmarkReducer
-});
-
-export default store;
