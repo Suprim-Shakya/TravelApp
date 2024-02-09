ec6752e75ccd896422b28c06dbbfa8d7cee39bb6
sauravkhanal <sauravkhanal635@gmail.com>
2024-02-09 19:58:48 +0545

    add icon on button

--- src/componentsSaurav/customComponents/CustomModal.jsx
+++ src/componentsSaurav/customComponents/CustomModal.jsx
@@ -5,7 +5,7 @@ import COLORS from '../../constants/colors';
 import Icon from "react-native-vector-icons/MaterialCommunityIcons"
 
 
-const CustomModal = ({ visible , header, text, onPress1, onPress2, title1, title2, danger1 = true, closeModal }) => {
+const CustomModal = ({ visible , header, text, onPress1, onPress2, title1, title2, icon1, icon2, danger1 = true, closeModal }) => {
     return (
         <Modal
             animationType="fade"
@@ -24,8 +24,8 @@ const CustomModal = ({ visible , header, text, onPress1, onPress2, title1, title
                     </View>
 
                     <View style={styles.btnContainer}>
-                        <SmallButton title={title1} onPress={onPress1} btnBgColor={danger1 && COLORS.danger} />
-                        <SmallButton title={title2} onPress={onPress2} />
+                        <SmallButton title={title1} onPress={onPress1} btnBgColor={danger1 && COLORS.danger} iconName={icon1}/>
+                        <SmallButton title={title2} onPress={onPress2} iconName={icon2} />
                     </View>
 
                     <Pressable style={styles.icon} onPress={closeModal}>
