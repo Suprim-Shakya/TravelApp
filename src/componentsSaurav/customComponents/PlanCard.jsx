48fb075cf5fd25ea25928efc99812fb3d7e31970
sauravkhanal <sauravkhanal635@gmail.com>
2024-02-09 13:26:33 +0545

    add on press

--- src/componentsSaurav/customComponents/PlanCard.jsx
+++ src/componentsSaurav/customComponents/PlanCard.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useDispatch } from 'react-redux';
 import CustomModal from './CustomModal';
 import COLORS from '../../constants/colors';
 
-const PlanCard = ({ name, index, deleteAction }) => {
+const PlanCard = ({ name, index, deleteAction, onPress = ()=> null }) => {
     const [modalVisible, setModalVisible] = useState(false);
     const dispatch = useDispatch();
 
@@ -14,28 +14,35 @@ const PlanCard = ({ name, index, deleteAction }) => {
     };
 
     return (
-        <View style={styles.view}>
-            <Text style={styles.text}>{index}. {name}</Text>
-            <Icon
-                name='delete-forever'
-                size={25}
-                style={styles.icon}
-                color={COLORS.danger}
-                onPress={handleDelete}
-            />
-            <CustomModal
-                visible={modalVisible}
-                text={`${name} will be removed from plan`}
-                title1="Ok"
-                title2="Cancel"
-                onPress1={() => {
-                    dispatch(deleteAction(name));
-                    setModalVisible(false);
-                }}
-                onPress2={() => setModalVisible(false)}
-                closeModal={() => setModalVisible(false)}
-                danger1={true}
-            />
+        <View style={styles.container}>
+
+            <Pressable 
+            style={styles.content} 
+            android_ripple={{ radius: 200, color: "rgba(0,0,0,0.2)" }}
+            onPress={onPress}
+            >
+                <Text style={styles.text}>{index}. {name}</Text>
+                <Icon
+                    name='delete-forever'
+                    size={25}
+                    style={styles.icon}
+                    color={COLORS.danger}
+                    onPress={handleDelete}
+                />
+                <CustomModal
+                    visible={modalVisible}
+                    text={`${name} will be removed from plan`}
+                    title1="Ok"
+                    title2="Cancel"
+                    onPress1={() => {
+                        dispatch(deleteAction(name));
+                        setModalVisible(false);
+                    }}
+                    onPress2={() => setModalVisible(false)}
+                    closeModal={() => setModalVisible(false)}
+                    danger1={true}
+                />
+            </Pressable>
         </View>
     );
 }
@@ -43,14 +50,19 @@ const PlanCard = ({ name, index, deleteAction }) => {
 export default PlanCard;
 
 const styles = StyleSheet.create({
-    view: {
-        minHeight: 50,
+    container: {
         backgroundColor: "white",
         marginHorizontal: 10,
         marginTop: 8,
-        paddingHorizontal: 10,
         elevation: 2,
         borderRadius: 10,
+        justifyContent: 'center',
+        overflow: 'hidden',
+    },
+    content: {
+        minHeight: 50,
+        paddingHorizontal: 10,
+        height: 'auto',
         display: 'flex',
         flexDirection: "row",
         alignItems: 'center',
@@ -62,7 +74,7 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
         fontSize: 18
     },
-    icon: {
-        backgroundColor: 'white'
-    }
+    // icon: {
+    //     backgroundColor: 'white',
+    // }
 });
