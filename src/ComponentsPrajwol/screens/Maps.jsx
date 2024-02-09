b7b5e779bff6fe82305e0a8a994193d27a0b6cf7
sauravkhanal <sauravkhanal635@gmail.com>
2024-02-09 19:57:16 +0545

    start default pin at KEC

--- src/ComponentsPrajwol/screens/Maps.jsx
+++ src/ComponentsPrajwol/screens/Maps.jsx
@@ -4,21 +4,25 @@ import MapView, { Marker } from 'react-native-maps';
 import CustomCallout from '../../componentsSaurav/customComponents/CustomCallout';
 
 const Maps = ({ route }) => {
-	const [selectedCoordinates, setSelectedCoordinates] = useState({ latitude: 0, longitude: 0 });
+	const [selectedCoordinates, setSelectedCoordinates] = useState({ latitude: 27.69917217030569, longitude: 85.29695607495043 });
 
 	const mapRef = useRef();
-	const [latitude, setLatitude] = useState()
-	const [longitude, setLongitude] = useState()
 
-	const { location, title, description } = route.params
+	const [details, setDetails] = useState({ title: "Kathmandu Engineering College", description: "" })
+
+	// let location, title, description
+
 	useEffect(() => {
+
 		if (route?.params && route.params?.location) {
+
+			const { location, title, description } = route.params
+
 			setSelectedCoordinates({
 				latitude: location.latitude,
 				longitude: location.longitude
 			});
-			setLatitude(location.latitude)
-			setLongitude(location.longitude)
+			setDetails({ title: title, description: description })
 		}
 	}, [route.params]);
 
@@ -36,30 +40,27 @@ const Maps = ({ route }) => {
 
 	return (
 		<View style={{ flex: 1 }}>
-			{Boolean(selectedCoordinates.latitude) &&
-				<MapView
-					ref={mapRef}
-					style={{ flex: 1 }}
-					initialRegion={{
-						latitude: latitude,
-						longitude: longitude,
-						latitudeDelta: 0.0922,
-						longitudeDelta: 0.0421,
-					}}
+			{/* {Boolean(selectedCoordinates.latitude) && */}
+			<MapView
+				ref={mapRef}
+				style={{ flex: 1 }}
+			>
+
+				<Marker
+					coordinate={{ latitude: selectedCoordinates.latitude, longitude: selectedCoordinates.longitude }}
+				// title={title}
+				// description={description}
 				>
+					{/* {
+						route.params?.title && */}
+						<CustomCallout
+							title={details.title }
+							description={details.description}
+						/>
+						{/* } */}
+				</Marker>
 
-					<Marker
-						coordinate={{ latitude: selectedCoordinates.latitude, longitude: selectedCoordinates.longitude }}
-						// title={title}
-						// description={description}
-					>
-					 	<CustomCallout
-								title={title}
-								description={description}
-							/>
-						</Marker>
-
-					{/* <Marker
+				{/* <Marker
 					coordinate={{
 						latitude: 27.688055,
 						longitude: 85.327595,
@@ -72,8 +73,8 @@ const Maps = ({ route }) => {
 					description="Marker Description saurav khanal is my name what is yours"
 					/>
 				</Marker> */}
-				</MapView>
-			}
+			</MapView>
+
 		</View>
 	);
 };
