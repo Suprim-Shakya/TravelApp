f72512852628ee0a457b7fb4dc8d967f00fab1ef
sauravkhanal <sauravkhanal635@gmail.com>
2024-02-12 15:02:39 +0545

    add activity data

--- src/constants/activities.js
+++ src/constants/activities.js
@@ -1,37 +1,152 @@
+// const activities = [
+//     {
+//       id: "1",
+//       name: "Kathmandu Fun Park",
+//       image: require('../assets/funpark.jpg'),
+//       details:
+//       "Kathmandu Fun Park......",
+//       coordinates:{
+//         latlng:{
+//           latitude:27.7015441,
+//           longitude:85.3186343,
+//         }
+//       },
+//       plusCode:`P82C+H52, Pradarshani Marg,Kathmandu 44600`
+      
+//     },
+//     {
+//       id: "2",
+//       name: "Garden of Dreams",
+//       image: require('../assets/garden.jpg'),
+//       details:
+//         `The Garden of Dreams, a neo classical historical garden, is situated in the midst of Kathmandu city,
+//          Nepal. The Garden's design has much in common with formal European gardens: paved perimeter paths,
+//           punctuated by pavilions, trellises, and various planting areas, surrounded by a sunken flower garden with a l
+//           arge pond at its center. It is an architectural landscape that encourages the visitor to stroll around and discover
+//            the Garden's treasures from many different vantage points.`,
+//            coordinates:{
+//             latlng:{
+//               latitude:123,
+//               longitude:345
+//             }
+//           },
+//         },
+//     {
+//       id: "3",
+//       name: "Skywalk Tower",
+//       image: require('../assets/skywalktower.jpg'),
+//       details:
+//         "The Skywalk Tower Kathmandu allows 500 people aboard at a time. However, the company plans to let in only 250 to 300 people in the initial phase. The tower uses 33 mm steel which is a British standard, Shrestha states. Stressing safety, he assures,  “There is no doubt about safety, even the Kathmandu Metropolitan City (KMC) and Tourism Department have given permission only after repeated inspections."
+//     },
+//     {
+//       id: "4",
+//       name: "Archery",
+//       image: require('../assets/archery.jpg'),
+//       details:
+//         "Interested in bow and arrow? Then you must visit this place.",
+//         coordinates:{
+//           latlng:{
+//             latitude:27.7399759,
+//             longitude:85.2293993
+//           }
+//         },
+//     },
+//     {
+//       id: "5",
+//       name: "Zoo",
+//       image: require('../assets/zoo.jpg'),
+//       details: "Description for Activity 1"
+//     }
+//   ];
+// export default activities;
+
 const activities = [
-    {
-      id: "1",
-      name: "Kumari Ghar",
-      image: require('../assets/kumari.jpg'),
-      details:
-        "The Kumari (literally “virgin”) is believed to be the reincarnation of the Hindu warrior goddess Taleju, a manifestation of Durga. Buddhists, on the other hand, believe her to be the tantric goddess Vajradevi. Nepal has several Kumaris, but Kumari Devi, or the Royal Kumari, is the most famous. Selected in early childhood from the Newar Buddhist Sakhya caste of goldsmiths and silversmiths, potential candidates are placed in a darkened room with freshly severed buffalo heads and dancing men wearing demon masks. It is believed if one candidate is not frightened, as most young girls surely would be, she is the next reincarnation of Taleju. Once chosen, she moves into the Kumari Ghar, where her feet will never touch the ground and she’ll only leave during specific festivals and religious duties.Each day, however, she makes occasional appearances from her balcony where crowds below wait to see her. As soon as she begins menstruating, or loses blood from an injury, she reverts back to mortal status and re-enters society, as the selection process begins again.The Kumari Ghar was built in 1757 by King Jaya Prakash Malla when he was overcome in guilt after offending the current Royal Kumari. In an act of atonement, he built her a new home. It was renovated in 1966."
-    },
-    {
-      id: "2",
-      name: "Garden of Dreams",
-      image: require('../assets/garden.jpg'),
-      details:
-        "The Garden of Dreams, a neo classical historical garden, is situated in the midst of Kathmandu city, Nepal. The Garden's design has much in common with formal European gardens: paved perimeter paths, punctuated by pavilions, trellises, and various planting areas, surrounded by a sunken flower garden with a large pond at its center. It is an architectural landscape that encourages the visitor to stroll around and discover the Garden's treasures from many different vantage points."
-    },
-    {
-      id: "3",
-      name: "Skywalk Tower",
-      image: require('../assets/skywalktower.jpg'),
-      details:
-        "The Skywalk Tower Kathmandu allows 500 people aboard at a time. However, the company plans to let in only 250 to 300 people in the initial phase. The tower uses 33 mm steel which is a British standard, Shrestha states. Stressing safety, he assures,  “There is no doubt about safety, even the Kathmandu Metropolitan City (KMC) and Tourism Department have given permission only after repeated inspections."
-    },
-    {
-      id: "4",
-      name: "Archery",
-      image: require('../assets/archery.jpg'),
-      details:
-        "Interested in bow and arrow? Then you must visit this place."
-    },
-    {
-      id: "5",
-      name: "Zoo",
-      image: require('../assets/OIP.jpg'),
-      details: "Description for Activity 1"
+  {
+    id: "1",
+    name: "Skywalk Tower",
+    image: require('../assets/skywalktower.jpg'),
+    details:
+      `The Skywalk Tower Kathmandu allows 500 people aboard at a time. However, the company plans to let in only 250 to 300 people in the initial phase.
+      The tower uses 33 mm steel which is a British standard, Shrestha states. Stressing safety, he assures,  
+      “There is no doubt about safety, even the Kathmandu Metropolitan City (KMC) and Tourism Department have given permission only 
+      after repeated inspections.`,
+      coordinates:{
+      latlng:{
+      latitude:27.7084751583111, 
+      longitude: 85.31921430876646
+      }
+},
+  },
+  {
+    id: "2",
+    name: "Garden of Dreams",
+    image: require('../assets/garden.jpg'),
+    details:
+      `The Garden of Dreams, a neo classical historical garden, is situated in the midst of Kathmandu city,
+       Nepal. The Garden's design has much in common with formal European gardens: paved perimeter paths,
+        punctuated by pavilions, trellises, and various planting areas, surrounded by a sunken flower garden with a l
+        arge pond at its center. It is an architectural landscape that encourages the visitor to stroll around and discover
+         the Garden's treasures from many different vantage points.`,
+         coordinates:{
+          latlng:{
+            latitude:123,
+            longitude:345
+          }
+        },
+      },
+  {
+    id: "3",
+    name: "Kumari Ghar",
+    image: require('../assets/kumari.jpg'),
+    details:
+      `The Kumari (literally “virgin”) is believed to be the reincarnation of the Hindu warrior goddess Taleju, a manifestation of Durga. Buddhists, on the other hand, believe her to be the tantric goddess Vajradevi. Nepal has several Kumaris, but Kumari Devi, or the Royal Kumari, is the most famous. Selected in early childhood from the Newar Buddhist Sakhya caste of goldsmiths and silversmiths, potential candidates are placed in a darkened room with freshly severed buffalo heads and dancing men wearing demon masks. It is believed if one candidate is not frightened, as most young girls surely would be, she is the next reincarnation of Taleju. Once chosen, she moves into the Kumari Ghar, where her feet will never touch the ground and she’ll only leave during specific festivals and religious duties.Each day, however, she makes occasional appearances from her balcony where crowds below wait to see her. As soon as she begins menstruating, or loses blood from an injury, she reverts back to mortal status and re-enters society, as the selection process begins again.The Kumari Ghar was built in 1757 by King Jaya Prakash Malla when he was overcome in guilt after offending the current Royal Kumari. In an act of atonement, he built her a new home. It was renovated in 1966.`,
+      coordinates:{
+          latlng:{
+              latitude: 27.70397814670256, 
+              longitude: 85.3064824241114
+          }
+      }
+  },
+  {
+    id: "4",
+    name: "Archery",
+    image: require('../assets/archery.jpg'),
+    details:
+      `Archery in Nepal has its roots in hunting, with Nepalese archers using bamboo bows and arrows. 
+      However, the sport slipped into rural life and became less popular over time. Today, sporting archery is making a comeback in Nepal,
+       with several archery clubs established in the Kathmandu valley  . The sport requires patience and dedication to master, 
+       and it challenges aiming skills.`,
+      coordinates:{
+          latlng:{
+              latitude:27.7399759,
+              longitude:85.2293993
+          }
+      }
+  },
+  {
+    id: "5",
+    name: "Zoo",
+    image: require('../assets/zoo.jpg'),
+    details: `The Central Zoo, also known as the National Trust for Nature Conservation (NTNC) Central Zoo, is the only zoo in Nepal. It is located in Jawalakhel, Lalitpur, in the Kathmandu Valley  . The zoo was originally established as a private zoo in 1932 by the late Rana Prime Minister Juddha Samsher and later came under the ownership of the Government of Nepal in 1950 . In 1995, the management of the zoo was entrusted to the NTNC for a period of 30 years .
+    The Central Zoo covers an area of 6 hectares (15 acres) and is home to approximately 870 animals from 109 different species . It offers visitors the opportunity to see a diverse range of animals, including rare and endangered species such as the Himalayan Blue Sheep, One-Horned Rhino, and Bengal Tiger .
+    
+    
+    The zoo provides various facilities and attractions for visitors. These include an information counter, souvenir shop, paddleboat and rafting activities, picnic sheds, children's playground with swings and merry-go-rounds, a visitor center with audiovisual facilities, a library, and an aquarium with sub-tropical fish.
+    
+    
+    While the Central Zoo offers a chance to see a variety of animals, there have been concerns about the conditions of some animal enclosures and the overall management of the zoo. Visitors have reported issues such as enclosures not being suited to the natural habitat of the animals, broken facilities, and a lack of proper care for the animals  . However, efforts are being made by dedicated zoo staff to improve the conditions and the welfare of the animals .
+    
+    
+    The Central Zoo attracts a significant number of visitors, with over one million people visiting annually . It is a popular destination for families and tourists looking to experience wildlife in Nepal. The zoo also offers a mini boat cruise on its on-site lake, providing additional recreational activities for visitors .
+    
+    
+    In conclusion, the Central Zoo in Nepal is the country's only zoo and is located in Jawalakhel, Lalitpur. It houses a diverse range of animals and offers various facilities and attractions for visitors. While there have been concerns about the conditions of some enclosures, efforts are being made to improve the zoo's management and the welfare of the animals.`,
+    coordinates:{
+      latlng:{
+          latitude:27.672836998778497, 
+          longitude: 85.31178647992881
+      }
     }
-  ];
+  }
+];
 export default activities;
