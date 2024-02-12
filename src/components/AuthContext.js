965d339ce3bd89acc6f061b71ec4a66c4e04958f
sauravkhanal <sauravkhanal635@gmail.com>
2024-02-12 13:57:05 +0545

    add activity indicator while checking auth state

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -1,10 +1,24 @@
-import React, { createContext, useContext, useState } from 'react';
-import { setAccessToken } from '../componentsSaurav/modules/handleAccessToken';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { getAccessToken, setAccessToken } from '../componentsSaurav/modules/handleAccessToken';
+import LoadingScreen from '../componentsSaurav/screens/LoadingScreen';
 
 
 const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
+    const [loading, setLoading] = useState(true);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(()=> {
+        async function checkAccessToken() {
+            const accessToken = await getAccessToken()
+            if (accessToken) {
+                setIsLoggedIn(true)
+            }
+            setLoading(false)
+        }
+
+        checkAccessToken();
+    },[])
 
     const login = () => {
         setIsLoggedIn(true);
@@ -14,6 +28,11 @@ export const AuthProvider = ({ children }) => {
         await setAccessToken(null)
         setIsLoggedIn(false);
     };
+
+    if (loading) {
+        // Display loading indicator or screen until the authentication status is determined
+        return <LoadingScreen />;
+    }
 
     return (
         <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
