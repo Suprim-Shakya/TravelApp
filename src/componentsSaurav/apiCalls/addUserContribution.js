df651dbc0bea8ec7a5a802d767de00cecb681f99
sauravkhanal <sauravkhanal635@gmail.com>
2024-02-12 10:06:49 +0545

    add timeout for 30s

--- src/componentsSaurav/apiCalls/addUserContribution.js
+++ src/componentsSaurav/apiCalls/addUserContribution.js
@@ -12,18 +12,27 @@ export default async function addUserContribution(formData) {
         return false
     }
 
-    
+
     try {
+
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        const timer = setTimeout(()=> {
+            controller.abort();
+        }, 30000)
+
         const res = await fetch(CONTRIBUTION_ENDPOINT, {
             method: "POST",
             body: formData,
             headers: {
                 "Content-Type": "multipart/form-data",
                 "Authorization": `Bearer ${accessToken}`
-            }
+            },
+            signal: signal
         });
 
-
+        clearTimeout(timer)
         const responseData = await res.json();
         // console.log(responseData)
         const message = responseData.message || "Contribution added successfully!";
@@ -31,12 +40,22 @@ export default async function addUserContribution(formData) {
             Alert.alert("Success", `${message} \nThe place is now discoverable.`)
             return true
         } else {
-            Alert.alert("Oops", `${message} \nBe sure to check places before adding.`)
-            return false
+            if (res.status === 401) {
+                Alert.alert("Oops", `Your Login Session has ended. \nPlease Login again to contribute.`)
+                return false
+            } else {
+
+                Alert.alert("Oops", `${message} \nBe sure to check places before adding.`)
+                return false
+            }
         }
 
 
     } catch (error) {
+        if (error.name === "AbortError") {
+            Alert.alert('Timeout', 'Form submission timed out. Please try again later')
+            return false;
+        }
         console.error("Error adding contribution:", error);
         Alert.alert("Oops!", "An error occurred while adding the contribution.");
         return false;
