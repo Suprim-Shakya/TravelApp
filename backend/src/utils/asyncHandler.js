4197414fa0c1df7905a8122388bb82f0c89d7353
sauravkhanal <sauravkhanal635@gmail.com>
2024-02-13 16:37:15 +0545

    mask error @ backend to response

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,8 +1,10 @@
+import ApiError from "./ApiError.js";
+
 const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((error) => {
-            console.error("Async Handler Error:", error);
-            next(error);
+            console.error("Async Handler Error: ", error);
+            res.status(500).json(new ApiError(500, "An unexpected error occurred."));
         });
     };
 };
