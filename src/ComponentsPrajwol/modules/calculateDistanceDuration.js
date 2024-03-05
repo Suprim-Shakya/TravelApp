c0d9345829c55db5dc64269dca6da99241cd4c38
sauravkhanal <sauravkhanal635@gmail.com>
2024-03-05 17:38:58 +0545

    add api

--- src/ComponentsPrajwol/modules/calculateDistanceDuration.js
+++ src/ComponentsPrajwol/modules/calculateDistanceDuration.js
@@ -1,5 +1,7 @@
+import { MAPS_API_KEY } from "../../componentsSaurav/config";
+
 export default async function calculateDistanceDuration(origin, destination, mode = "driving") {
-    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin.latitude},${origin.longitude}&destinations=${destination.latitude},${destination.longitude}&mode=${mode}&key=AIzaSyCL05Qyt9d9tmnFeC4rjaJNbaSqN1tIRjw`;
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin.latitude},${origin.longitude}&destinations=${destination.latitude},${destination.longitude}&mode=${mode}&key=${MAPS_API_KEY}`;
 
     // console.log("[calculate Distance Duration] The data I received is: ", origin);
 
